aa783a0d5f5c95f514f1c3dd3fe264445e10e880
Gukisz <gustavoribeiro070@gmail.com>
2024-11-27 20:18:01 -0300

    ADD: Adicionando especificações de novas armas Adicionada novas armas ao código além de suas especificações

--- bob_do_job.js
+++ bob_do_job.js
@@ -1,9 +1,34 @@
-const ask = require('readline-sync')
+const ask = require('readline-sync');
+
 let beretta = {
-    height : 137, 
-    width : 38,
-    lenght : 217,
-    mass : 970,
-    caliber : 9,
-  
-}
+    height: 137, 
+    width: 38,
+    length: 217,
+    mass: 970,
+    caliber: 9,
+};
+
+let glock17 = {
+    height: 138,
+    width: 30,
+    length: 186,
+    mass: 625,
+    caliber: 9,
+};
+
+let desertEagle = {
+    height: 160,
+    width: 40,
+    length: 270,
+    mass: 2000,
+    caliber: 50,
+};
+
+let ak47 = {
+    height: 260,
+    width: 70,
+    length: 880,
+    mass: 4300,
+    caliber: 7.62,
+};
+
